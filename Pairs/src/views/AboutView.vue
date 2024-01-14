e2e48d12b5d74fead5378880ad743ab4172c7edd
Name <you@gmail.com>
2024-01-14 15:38:59 +0500

    Removed unnecessary comments and unused code

--- Pairs/src/views/AboutView.vue
+++ Pairs/src/views/AboutView.vue
@@ -37,7 +37,7 @@ let arr = [
 
 const PictureArray = ref(arr)
 
-// Functions
+// FUNCTIONS
 
 function sentToCompare(event) {
   if (!checkMantched(event.target.style.backgroundImage.slice(4, -1).replace(/"/g, ''))) {
@@ -52,19 +52,7 @@ function sentToCompare(event) {
         compare.cards[1][1] !== compare.cards[0][1] &&
         compare.cards[1][0] === compare.cards[0][0]
       ) {
-        //находим элемент по классу
-        let firstCard = document.querySelector(`.${compare.cards[0][1].slice(5)}`)
-        let secondCard = document.querySelector(`.${compare.cards[1][1].slice(5)}`)
-
-        document.querySelector(`.${compare.cards[0][1].slice(5)}`).removeAttribute('onclick')
-        document.querySelector(`.${compare.cards[1][1].slice(5)}`).removeAttribute('onclick')
-
-        // найденому элементу меняем класс на класс с датой
-        firstCard.className = `${compare.cards[0][1]}`
-
-        secondCard.className = `${compare.cards[1][1]}`
-
-        // записываем новый класс элемента в массив
+        // add the address to the matches array
         matchedArr.push(compare.cards[1][0])
         compare.cards = []
         console.log('есть совпадение')
@@ -80,9 +68,6 @@ function sentToCompare(event) {
       }
     }
   }
-
-  // console.log((event.target.style.webkitFilter = 'brightness(1)'))
-  // console.log(event.target.style.backgroundImage.slice(4, -1).replace(/"/g, ''))
 }
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5)
@@ -101,66 +86,19 @@ function checkMantched(url) {
   display: grid;
   grid-template-columns: 100px 100px 100px;
   grid-template-rows: 200px 200px;
-  /* grid-template-areas: 'a a a' 'b c c' 'b c c'; */
+
   gap: 5px;
-  /* justify-items: center;
-  align-items: start; */
+
   justify-content: center;
-  /* grid-auto-flow: column; */
 }
 
 .item {
-  /* height: 50px;
-  width: 40px; */
   filter: brightness(0);
   background: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5));
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   background-blend-mode: normal;
-}
-.item_0 {
-  /* justify-self: center;
-  align-self: center; */
-  /* 
-  background-image: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5)),
-    url('https://w7.pngwing.com/pngs/557/517/png-transparent-tekken-6-bloodline-rebellion-ling-xiaoyu-tekken-3-tekken-king-orange-video-game.png');
-   */
-}
-.item_1 {
-  /* justify-self: center;
-  align-self: center; */
-
-  /* background-image: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5)),
-    url('https://e7.pngegg.com/pngimages/149/227/png-clipart-tekken-5-dark-resurrection-tekken-4-ling-xiaoyu-kazuya-mishima-tekken-video-game-shoe.png'); */
-}
-.item_2 {
-  /* justify-self: center;
-  align-self: center; */
-
-  /* background-image: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5)),
-    url('https://e7.pngegg.com/pngimages/149/227/png-clipart-tekken-5-dark-resurrection-tekken-4-ling-xiaoyu-kazuya-mishima-tekken-video-game-shoe.png'); */
-}
-.item_3 {
-  /* justify-self: center;
-  align-self: center; */
-
-  /* background-image: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5)),
-    url('https://e7.pngegg.com/pngimages/149/227/png-clipart-tekken-5-dark-resurrection-tekken-4-ling-xiaoyu-kazuya-mishima-tekken-video-game-shoe.png'); */
-}
-.item_4 {
-  /* justify-self: center;
-  align-self: center; */
-
-  /* background-image: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5)),
-    url('https://e7.pngegg.com/pngimages/149/227/png-clipart-tekken-5-dark-resurrection-tekken-4-ling-xiaoyu-kazuya-mishima-tekken-video-game-shoe.png'); */
-}
-.item_5 {
-  /* justify-self: center;
-  align-self: center; */
-
-  /* background-image: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5)),
-    url('https://e7.pngegg.com/pngimages/149/227/png-clipart-tekken-5-dark-resurrection-tekken-4-ling-xiaoyu-kazuya-mishima-tekken-video-game-shoe.png'); */
 }
 
 @media (min-width: 1024px) {
